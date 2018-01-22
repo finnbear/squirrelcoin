3317c5b7f6b98ce9663f0e8b9f459b2de76689d2
finnbear <finnbearone@gmail.com>
2018-01-21 19:15:27 -0800

    Fixed math error.

--- src/community-reward.js
+++ src/community-reward.js
@@ -17,7 +17,7 @@ module.exports = function() {
 			let rewardAmount = tx.to[0].amount;
 
 			let treasuryPayout = tx.to[1];
-			let expectedTreasuryAmount = Math.floor(rewardAmount * config.treasuryPercent / 100);
+			let expectedTreasuryAmount = Math.floor(rewardAmount * config.treasuryPercent);
 
 			if (treasuryPayout.amount !== expectedTreasuryAmount) {
 				throw Error("Oracle must pay " + (config.treasuryPercent * 100) + "% of reward to treasury (" + (expectedTreasuryAmount / config.oneCoin) + " instead of " + (treasuryPayout.amount / config.oneCoin) + ")");
@@ -28,7 +28,7 @@ module.exports = function() {
 			}
 
 			let founderPayout = tx.to[0];
-			let expectedFounderAmount = Math.floor(rewardAmount * config.founderPercent / 100);
+			let expectedFounderAmount = Math.floor(rewardAmount * config.founderPercent);
 
 			if (founderPayout.amount !== expectedFounderAmount) {
 				throw Error("Oracle must pay " + (config.founderPercent * 100) + "% of reward to founder (" + (expectedFounderAmount / config.oneCoin) + " instead of " + (founderPayout.amount / config.oneCoin) + ")");
