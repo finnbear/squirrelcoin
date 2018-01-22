046b7fcbfb7355c927126f9bafd35bdbfda0220f
finnbear <finnbearone@gmail.com>
2018-01-21 19:13:24 -0800

    More readable community validation errors.

--- src/community-reward.js
+++ src/community-reward.js
@@ -20,7 +20,7 @@ module.exports = function() {
 			let expectedTreasuryAmount = Math.floor(rewardAmount * config.treasuryPercent / 100);
 
 			if (treasuryPayout.amount !== expectedTreasuryAmount) {
-				throw Error("Oracle must pay " + (config.treasuryPercent * 100) + "% of reward to treasury (" + expectedTreasuryAmount + " isntead of " + treasuryPayout.amount + ")");
+				throw Error("Oracle must pay " + (config.treasuryPercent * 100) + "% of reward to treasury (" + (expectedTreasuryAmount / config.oneCoin) + " instead of " + (treasuryPayout.amount / config.oneCoin) + ")");
 			}
 
 			if (treasuryPayout.address != "treasury") {
@@ -31,7 +31,7 @@ module.exports = function() {
 			let expectedFounderAmount = Math.floor(rewardAmount * config.founderPercent / 100);
 
 			if (founderPayout.amount !== expectedFounderAmount) {
-				throw Error("Oracle must pay " + (config.founderPercent * 100) + "% of reward to founder (" + expectedFounderAmount + " instead of " + founderPayout.amount + ")");
+				throw Error("Oracle must pay " + (config.founderPercent * 100) + "% of reward to founder (" + (expectedFounderAmount / config.oneCoin) + " instead of " + (founderPayout.amount / config.oneCoin) + ")");
 			}
 
 			if (founderPayout.address != config.founderAddress) {
